1194cfcf9b465a234b5cfe80cf131b261c489a96
Nikaaa001 <99095382+Nikaaa001@users.noreply.github.com>
2023-10-19 02:06:05 +0400

    feat: add todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && inputValue.trim() !== '') {
+      setTasks([...tasks, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const addTask = () => {
+    if (inputValue.trim() !== '') {
+      setTasks([...tasks, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+
+
 
   return (
     <>
-     
+      <div className="w-[327px] mx-auto mt-[48px]">
+
+        <div className="flex w-full h-[20px] items-center justify-between">
+
+          <h1 className="text-20px text-[#FFF]">TODO</h1>
+          <img src="./images/icon-moon.svg" alt="Moon" />
+
+        </div>
+
+        <label className="flex w-full h-[64px] bg-[#FFF] justify-between items-center pl-[20px] pr-[20px] mt-[40px] rounded-[5px] shadow-shadowConts">
+          
+          <input type="checkbox"/>
+          <input type="email" placeholder='Create a new todo…' className=' outline-none' value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyPress} />
+
+        </label>
+
+        <div className="relative w-full h-[368px] bg-[#FFF] mt-[16px] shadow-shadowConts rounded-[5px] overflow-auto">
+          <div>
+            {tasks.map((task, index) => (
+                <div key={index} className="flex flex-col items-center justify-between">
+                  <label className="flex w-full h-[48px] bg-[#FFF] justify-between items-center pl-[20px] pr-[20px] rounded-[5px] shadow-shadowConts">
+                    <input type="checkbox"/>
+                    <span>{task}</span>
+                    <img src="./images/icon-cross.svg" alt="Cross" onClick={() => removeTask(index)}/>
+                </label>
+                <hr className='w-full h-[2px]'/>
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="absolute w-[327px] h-[48px] bottom-[110px] bg-[#FFF]">
+            <hr className='w-full h-[2px]'/>
+            <div className="w-full h-[48px] flex items-center justify-between px-[20px]">
+              <span>items left</span>
+              <span>Clear Completed</span>
+            </div>
+          </div>
+
+        <div className="w-full h-[48px] bg-[#FFF] shadow-shadowConts mt-[16px] flex justify-between items-center">
+
+          <div className="w-[166px] h-[18px] flex justify-between items-center mx-auto">
+            <span className=' font-josefin text-active text-14px font-bold'>All</span>
+            <span className='font-josefin text-active text-14px font-bold'>Active</span>
+            <span className='font-josefin text-active text-14px font-bold'>Completed</span>
+          </div>
+
+        </div>
+
+      </div>
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-blackOne': '#14131B',
+        'active': '#9495A5',
+      },
+      fontFamily: {
+        josefin: "''Josefin Sans', sans-serif"
+      },
+      fontSize: {
+        '20px': '20px',
+        '40px': '40px',
+        '14px': '14px',
+      },
+      boxShadow: {
+        'shadowConts': '0px 35px 50px -15px rgba(194, 195, 214, 0.50)',
+      }
+    },
   },
   plugins: [],
 }
+
+// font-family: 'Josefin Sans', sans-serif;
 
